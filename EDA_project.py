276877804ee76405038dc866eb858cb269e70805
Gideon <gideonkarasek@gmail.com>
2019-11-06 12:43:21 -0600

    Add eda project to repo

---
+++ EDA_project.py
@@ -0,0 +1,14 @@
+# import necessary libraries
+import pandas as pd
+import numpy as np
+
+# import the csv file as a DataFrame
+df = pd.read_csv('train.csv')
+
+
+df.head()
+
+df.info()
+
+df.describe()
+
